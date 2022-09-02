3eaa19d51d46e90f007babb283a3896a6463c26f
ajstover07 <amandajstover@gmail.com>
2022-09-02 13:36:58 -0400

    commit tree 0e4ee7a413c97bee8fb110d213166d63633c1a1c

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// code your solution here
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+
+function superbowlWin(yearsAndResults){
+    const result = yearsAndResults.find(record => record.result === "W");
+    return result ? result.year : undefined
+}
+
+
+// function superbowlWin(yearsAndResults){
+//     return yearsAndResults.find(isWin).year
+// }
+
+// function isWin(gameData){
+//     // let result = undefined
+//     //  for (let year of record){
+//     //     if (gameData.result === "W" ){
+//     //         result = year
+//     //     }
+//     //  }
+//      if (gameData.result ==="W") {
+//          return true;
+//     } else {
+//          return false;
+//     }
+//  }
+
+
+
